e83ca9d7f2c2762411d8c53bd5415fe42364a572
Rey <sm8967724231@gmail.com>
2022-02-12 01:50:34 +0530

    actions update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Window.initialize();
   await SystemTheme.accentInstance.load();
-  darkMode = await SystemTheme.darkMode;
   if (Platform.isWindows) {
     await Window.hideWindowControls();
     doWhenWindowReady(() {
